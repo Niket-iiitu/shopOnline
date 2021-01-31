b1ce760ec7682dec80e0ee622e7e2ce2895b04bc
adi <19106@iiitu.ac.in>
2021-01-31 16:26:04 +0530

    trial 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import SignUp from "./pages/signUp";
 import SignIn from "./pages/signin";
 import header from "./pages/header";
 import productpage from "./pages/productPage";
+
+
 import React from "react";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 const App = () => {
@@ -13,6 +15,7 @@ const App = () => {
           <Route path="/SignIn" component={SignIn} exact />
           <Route path="/home" component={header} exact />
           <Route path="/product" component={productpage} exact />
+          
         </main>
       </div>
     </BrowserRouter>
@@ -22,3 +25,4 @@ const App = () => {
 export default App;
 
 //background-image: url(https://opconventioncenter.com/wp-content/uploads/2018/02/EXPO-Inc.-tools.jpeg);
+//how about test
